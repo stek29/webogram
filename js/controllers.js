dd60be6abe4e24a1f81015ad7fb1bb3892dc673a
Viktor Oreshkin <imselfish@stek29.rocks>
2018-01-02 21:23:48 +0300

    0.7 upd

--- js/controllers.js
+++ js/controllers.js
@@ -1149,6 +1149,7 @@ angular.module('myApp.controllers', ['myApp.i18n'])
               var message = AppMessagesManager.getMessage(dialog.top_message)
               if (message.fromID > 0) {
                 wrappedDialog.peerID = message.fromID
+                wrappedDialog.foundInHistory = true
               }
             }
 
@@ -2628,8 +2629,14 @@ angular.module('myApp.controllers', ['myApp.i18n'])
       })
       var chatParticipantsPromise
       if (peerID < 0) {
-        chatParticipantsPromise = AppProfileManager.getChatFull(-peerID).then(function (chatFull) {
-          var participantsVector = (chatFull.participants || {}).participants || []
+        if (AppPeersManager.isChannel(peerID)) {
+          chatParticipantsPromise = AppProfileManager.getChannelParticipants(-peerID)
+        } else {
+          chatParticipantsPromise = AppProfileManager.getChatFull(-peerID).then(function (chatFull) {
+            return (chatFull.participants || {}).participants || []
+          })
+        }
+        chatParticipantsPromise = chatParticipantsPromise.then(function (participantsVector) {
           var ids = []
           angular.forEach(participantsVector, function (participant) {
             ids.push(participant.user_id)
